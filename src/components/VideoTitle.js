006e3dcafd5ec0da17e852ade56c9e1ae7904181
ayush-nautiyal16 <ayushnautiyal75098@gmail.com>
2025-11-07 13:00:03 +0530

    Built Main Container && VideoBackground

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const VideoTitle = ({title,overview}) => {
     return (
-        <div className='pt-36 px-12'>
+        <div className='w-screen aspect-video pt-[20%] px-14 absolute text-white bg-gradient-to-r from-black'>
             <h1 className='text-6xl font-bold'>{title}</h1>
             <p className='py-6 text-lg w-1/2'>{overview}</p>
             <div >
-                <button className='bg-gray-500 text-white p-4 px-16 text-xl bg-opacity-50 rounded-lg'> ▶️Play</button>
-                <button className='mx-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg'>More Info</button>
+                <button className='bg-slate-100 text-black p-4 px-16 text-xl  rounded-lg hover:bg-opacity-85'> ▶️Play</button>
+                <button className='mx-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg hover:bg-opacity-40'> More Info</button>
             </div>
         </div>
     )
