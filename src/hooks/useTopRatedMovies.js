e070b7b0dc8feb2690e0c556746767f7c01bd938
ayush-nautiyal16 <ayushnautiyal75098@gmail.com>
2025-11-10 01:00:41 +0530

    feat: Add multiple movie categories

---
+++ src/hooks/useTopRatedMovies.js
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import { API_OPTION } from "../utils/constant";
+import { addTopRatedMovies } from "../utils/movieSlice";
+import { useEffect } from "react";
+
+const useTopRatedMovies = () => {
+    const dispatch = useDispatch();
+    const getTopRatedMovies = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/top_rated?page=1', API_OPTION
+        );
+        const json = await data.json();
+        console.log(json);
+        dispatch(addTopRatedMovies(json.results));
+    }
+    useEffect(() => {
+        getTopRatedMovies();
+    }, []);
+}
+export default useTopRatedMovies;
