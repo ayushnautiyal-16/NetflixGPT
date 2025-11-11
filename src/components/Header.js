49709a7b0fa093e75ebcfb28039946672b591379
ayush-nautiyal16 <ayushnautiyal75098@gmail.com>
2025-11-11 22:09:38 +0530

    Creates GPT search button

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { addUser,removeUser } from '../utils/userSlice';
-import { LOGO } from '../utils/constant';
+import { LOGO, SUPPORTED_LANGUAGES } from '../utils/constant';
+import { toggleGptSearchView } from '../utils/gptslice';
+import { changeLanguage } from '../utils/configSlice';
 const Header = () => {
     const dispatch= useDispatch();
     const navigate = useNavigate();
@@ -30,6 +32,17 @@ const Header = () => {
         });
         return ()=> unsubscribe();
     }, [])
+
+    const handleGptSearchClick = ()=>{
+        // Toggle my GPT search
+        dispatch(toggleGptSearchView());
+    }
+
+    const handleLanguageChange=(e)=>{
+        dispatch(changeLanguage(e.target.value));
+        // console.log(e.target.value);
+    }
+
     return (
         <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
             <img
@@ -37,6 +50,12 @@ const Header = () => {
                 src={LOGO}
                 alt="Logo" />
                 {user && <div className='flex p-2'>
+                    <select className='p-2 m-2 bg-gray-900 text-white rounded-lg 'onChange={handleLanguageChange}>
+                        {SUPPORTED_LANGUAGES.map(lang=> <option key ={lang.identifier}value={lang.identifier}>{lang.name}</option>)}
+                    </select>
+                <button className='py-2 px-4 m-4 my-2 bg-purple-800 text-white rounded-lg'
+                onClick={handleGptSearchClick}
+                > GPT Search</button>
                     <img
                     className='w-12 h-12'
                     src={user?.photoURL} alt="user-icon" />
